37c200b5aaa49e10188f6d7dde903e77a51d5142
Narsis93 <106112306+Narsis93@users.noreply.github.com>
2023-03-12 21:20:30 +0330

    Add files via upload

---
+++ jquey/index.js
@@ -0,0 +1,49 @@
+ 
+$(document).ready(function(){
+    $(".b1").click(function(){
+        alert("YOU CHOOSE WRONG.");
+    });
+});
+$(document).ready(function(){
+    $(".b2").click(function(){
+        alert("YOU CHOOSE WRONG.");
+    });
+});
+$(document).ready(function(){
+    $(".b3").click(function(){
+        alert("YOU CHOOSE WRONG.");
+    });
+});
+$(document).ready(function(){
+    $(".b4").click(function(){
+        alert("YOU CHOOSE WRONG.");
+    });
+});
+$(document).ready(function(){
+    $(".b5").click(function(){
+        alert("YOU CHOOSE WRONG.");
+    });
+});
+$(document).ready(function(){
+    $("button.b6").click(function(){
+        alert("YOU win.");
+       });
+    });
+    $(document).ready(function(){
+        $(".b7").click(function(){
+            alert("YOU CHOOSE WRONG.");
+        });
+    });
+    $(document).ready(function(){
+        $(".b8").click(function(){
+            alert("YOU CHOOSE WRONG.");
+        });
+    });
+
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#p1").css("color", "pink")
+            .slideUp(2000)
+            .slideDown(2000);
+        });
+      });
